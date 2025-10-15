e84076111b844a41c3b9f587191cafa7456a0866
oreo-collab <okorieevan@gmail.com>
2025-10-15 08:09:44 +0100

    index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,14 @@ import 'normalize.css';
 import './defaults.css';
 import Shop from './Shop';
 
+// Optional: Pi SDK sandbox init
+window.PiNetwork = {
+  environment: 'sandbox'
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <Shop />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
